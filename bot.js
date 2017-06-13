2548803241020027007bd966c6081919ddffa3d2
Bougjdi <abdelhakim.bougjdi@gmail.com>
2017-06-13 03:30:37 +0200

    Create bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
-const Google_trad = require('./google_trans.js')
 var httpClient = require('node-rest-client-promise').Client()
 
 client.on('ready', () => {
@@ -14,22 +13,13 @@ client.on('message', msg => {
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
 
   // If message is hello, post hello too
-  if (msg.content === 'hi') {
-    console.log(' salam !')
-   msg.channel.send('salam')
-  }
-  if (msg.content !== '') {
   var b = msg.content
     httpClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q='+ b +'&APPID=b05787eda8d8f7967925692ea52134d2')
     .then((res) => {
       var tempKal = res.data.main.temp
       var tempCel = tempKal - 273.15
-      msg.channel.sendMessage('Il fait à '+ b +': ' + tempCel.toFixed(2) + ' °C')
-    })
-  }
-  //if(Google_trad.match(msg)){
-    // Google_trad.action(msg)
- // }
+     // msg.channel.sendMessage('Il fait à '+ b +': ' + tempCel.toFixed(2) + ' °C')
+      msg.channel.send('Il fait à '+ b +': ' + tempCel.toFixed(2) + ' °C')
 })
 
-client.login(config.token)
+client.login(config.token)})
